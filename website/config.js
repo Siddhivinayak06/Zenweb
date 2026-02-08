d5d3db70b57957d61cf0acb452e572cbc5e8e915
Siddhivinayak06 <siddhivinayaksawant04@gmail.com>
2026-02-08 18:26:17 +0530

    Securely ignore config files and add templates

--- website/config.js
+++
@@ -1,5 +0,0 @@
-globalThis.ZenWebConfig = {
-    SUPABASE_URL: 'https://vlguestftwnsvtdhfcbb.supabase.co',
-    SUPABASE_KEY: 'sb_publishable_dbFY_gWjVCzpecAZIfN3bA_Nkyg7eG_',
-    STRIPE_PAYMENT_LINK: 'https://buy.stripe.com/test_aFaaEX0xtez12ye9Pz14400' // Verified Payment Link
-};
